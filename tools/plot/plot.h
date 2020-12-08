a5be99a4da70b5547baef7603a1b692fce01cab2
Vitaly Yulis <vitaly.yulis@gmail.com>
2020-12-08 12:07:30 +0300

    Fix inline [ci skip]

--- tools/plot/plot.h
+++ tools/plot/plot.h
@@ -7,34 +7,34 @@
 namespace dsplib {
 namespace plot {
 
-void plot(const arr_real& x, const std::string& format = "") {
+inline void plot(const arr_real& x, const std::string& format = "") {
     std::vector<real_t> tx(x.data(), x.data()+x.size());
     matplotlibcpp::plot(tx, format);
 }
 
-void plot(const arr_real& x, const arr_real& y, const std::string& format = "") {
+inline void plot(const arr_real& x, const arr_real& y, const std::string& format = "") {
     std::vector<real_t> tx(x.data(), x.data()+x.size());
     std::vector<real_t> ty(y.data(), y.data()+y.size());
     matplotlibcpp::plot(tx, ty, format);
 }
 
-void scatterplot(const arr_real& x, const arr_real& y) {
+inline void scatterplot(const arr_real& x, const arr_real& y) {
     std::vector<real_t> tx(x.data(), x.data()+x.size());
     std::vector<real_t> ty(y.data(), y.data()+y.size());
     matplotlibcpp::scatter(tx, ty);
 }
 
-void scatterplot(const arr_cmplx& x) {
+inline void scatterplot(const arr_cmplx& x) {
     auto tx = real(x);
     auto ty = imag(x);
     scatterplot(tx, ty);
 }
 
-void title(const std::string& title) {
+inline void title(const std::string& title) {
     matplotlibcpp::title(title);
 }
 
-void show() {
+inline void show() {
     matplotlibcpp::show();
 }
 
