45c0195f35c8a8ff00abd1b3f1ff9b917fbddbe8
Vitaly Yulis <vitaly.yulis@gmail.com>
2021-08-02 21:22:40 +0300

    Fixed fir (the compiler is smarter). Fix CI.

--- lib/fir.cpp
+++ lib/fir.cpp
@@ -19,21 +19,21 @@ fir_cmplx::fir_cmplx()
 }
 
 //-------------------------------------------------------------------------------------------------
-fir::fir(const arr_real &h)
+fir::fir(const arr_real& h)
 {
     _h = h;
-    _d = zeros(h.size()-1);
+    _d = zeros(h.size() - 1);
 }
 
 //-------------------------------------------------------------------------------------------------
-fir_cmplx::fir_cmplx(const arr_cmplx &h)
+fir_cmplx::fir_cmplx(const arr_cmplx& h)
 {
     _h = h;
-    _d = arr_cmplx(zeros(h.size()-1));
+    _d = arr_cmplx(zeros(h.size() - 1));
 }
 
 //-------------------------------------------------------------------------------------------------
-arr_real fir::process(const arr_real &s)
+arr_real fir::process(const arr_real& s)
 {
     auto x = concatenate(_d, s);
 
@@ -49,7 +49,7 @@ arr_real fir::process(const arr_real &s)
 }
 
 //-------------------------------------------------------------------------------------------------
-arr_cmplx fir_cmplx::process(const arr_cmplx &s)
+arr_cmplx fir_cmplx::process(const arr_cmplx& s)
 {
     auto x = concatenate(_d, s);
 
@@ -65,54 +65,44 @@ arr_cmplx fir_cmplx::process(const arr_cmplx &s)
 }
 
 //-------------------------------------------------------------------------------------------------
-const arr_real &fir::impz() const
+const arr_real& fir::impz() const
 {
     return _h;
 }
 
 //-------------------------------------------------------------------------------------------------
-const arr_cmplx &fir_cmplx::impz() const
+const arr_cmplx& fir_cmplx::impz() const
 {
     return _h;
 }
 
 //-------------------------------------------------------------------------------------------------
-template <class T>
-static void _conv(const T* x, const T* h, T* r, int nh, int nx)
+template<class T>
+static void _conv(const T* __restrict__ x, const T* __restrict__ h, T* __restrict__ r, int nh, int nx)
 {
-    int nr = nx - nh + 1;
-    const T* px = x;
-    const T* ph = h;
-    T* pr = r;
-    for (int i=0; i < nr; ++i)
-    {
-        pr = r + i;
-        px = x + i;
-        ph = h + nh - 1;
-        *pr = 0;
-        for (int k=0; k < nh; ++k)
-        {
-            *pr += (*px) * (*ph);
-            ++px;
-            --ph;
+    const int nr = nx - nh + 1;
+    for (int i = 0; i < nr; ++i) {
+        r[i] = 0;
+        for (int k = 0; k < nh; ++k) {
+            r[i] += x[i + k] * h[nh - k - 1];
         }
     }
 }
 
 //-------------------------------------------------------------------------------------------------
-arr_real fir::conv(const arr_real &x, const arr_real &h)
+arr_real fir::conv(const arr_real& x, const arr_real& h)
 {
     arr_real r(x.size() - h.size() + 1);
-    _conv <real_t> (x.data(), h.data(), r.data(), h.size(), x.size());
+    _conv<real_t>(x.data(), h.data(), r.data(), h.size(), x.size());
     return r;
 }
 
 //-------------------------------------------------------------------------------------------------
-arr_cmplx fir_cmplx::conv(const arr_cmplx &x, const arr_cmplx &h)
+arr_cmplx fir_cmplx::conv(const arr_cmplx& x, const arr_cmplx& h)
 {
     arr_cmplx r(x.size() - h.size() + 1);
-    _conv <cmplx_t> (x.data(), h.data(), r.data(), h.size(), x.size());
+    _conv<cmplx_t>(x.data(), h.data(), r.data(), h.size(), x.size());
     return r;
 }
 
-}   ///< dsplib
+}   // namespace dsplib
