d0199274778c09e233a981e830a52369f2f4bebc
Vitaly Yulis <vitaly.yulis@gmail.com>
2022-09-02 23:18:41 +0300

    separate example project

--- cmake/bastard_setup.cmake
+++ cmake/bastard_setup.cmake
@@ -1,4 +1,8 @@
 # https://github.com/vitalsong/bastard
-set(BASTARD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake/bastard")
+
+if (NOT DEFINED BASTARD_DIR)
+    set(BASTARD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake/bastard")
+endif()
+
 include("${BASTARD_DIR}/bastard.cmake")
 message("Bastard version: ${BASTARD_VERSION}")
