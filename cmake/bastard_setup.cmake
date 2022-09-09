4c0942ffa6b64f8f7ff0ea37e951061c27f27638
Vitaly Yulis <43682603+vitalsong@users.noreply.github.com>
2022-09-10 00:32:19 +0300

    Removed bastard package manager (#7)

--- cmake/bastard_setup.cmake
+++
@@ -1,8 +0,0 @@
-# https://github.com/vitalsong/bastard
-
-if (NOT DEFINED BASTARD_DIR)
-    set(BASTARD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake/bastard")
-endif()
-
-include("${BASTARD_DIR}/bastard.cmake")
-message("Bastard version: ${BASTARD_VERSION}")
