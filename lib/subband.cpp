294e32f1a96103ad2c0fa19a40876807765ff4f6
Vitaly Yulis <43682603+vitalsong@users.noreply.github.com>
2025-04-23 13:33:23 +0300

    Channelizer/ChannelSynthesizer (#64)
    * Add polyphase FFT filter banks
    * Add `zadoff_chu` function
    * Update tests

---
+++ lib/subband.cpp
@@ -0,0 +1,302 @@
+#include "dsplib/subband.h"
+#include "dsplib/assert.h"
+#include "dsplib/resample.h"
+#include "dsplib/fft.h"
+#include "dsplib/ifft.h"
+
+// original FilterBanks sources: `https://github.com/kkumatani/distant_speech_recognition`
+
+// details: Digital Receivers and Transmitters Using Polyphase Filter Banks for Wireless Communications, 2003
+
+namespace dsplib {
+
+namespace {
+
+class MatView
+{
+public:
+    explicit MatView(const arr_real& x, int n, int m)
+      : n_{n}
+      , m_{m}
+      , data_{x.data()} {
+        DSPLIB_ASSERT(n * m == x.size(), "matrix size error");
+    }
+
+    //TODO: use span
+    const real_t* operator[](uint32_t i) const noexcept {
+        assert(i <= n_);
+        return data_ + (i * m_);
+    }
+
+private:
+    const int n_;
+    const int m_;
+    const real_t* data_;
+};
+
+class MutMatView
+{
+public:
+    explicit MutMatView(arr_real& x, int n, int m)
+      : n_{n}
+      , m_{m}
+      , data_{x.data()} {
+        DSPLIB_ASSERT(n * m == x.size(), "matrix size error");
+    }
+
+    const real_t* operator[](uint32_t i) const noexcept {
+        assert(i <= n_);
+        return data_ + (i * m_);
+    }
+
+    real_t* operator[](uint32_t i) noexcept {
+        assert(i <= n_);
+        return data_ + (i * m_);
+    }
+
+private:
+    const int n_;
+    const int m_;
+    real_t* data_;
+};
+
+class CircBuffer
+{
+public:
+    /**
+     * @brief Construct a circular buffer to keep samples periodically
+     * @details It keeps period arrays which is completely updated with the period 'period'.
+     * Each array holds actual values of the samples.
+     * @param len The size of each array
+     * @param period The period of the circular buffer
+     */
+    explicit CircBuffer(int len, int period)
+      : len_{len}
+      , period_{period}
+      , sample_idx_{period_ - 1}
+      , buf_(period_ * len_)
+      , smps_{buf_, period_, len_} {
+    }
+
+    //TODO: use span
+    const real_t* operator[](int time) const noexcept {
+        time = index_(time);
+        return smps_[time];
+    }
+
+    real_t* operator[](int time) noexcept {
+        time = index_(time);
+        return smps_[time];
+    }
+
+    void push(const arr_real& s, bool reverse = false) {
+        DSPLIB_ASSERT(s.size() == len_, "input size error");
+        sample_idx_ = (sample_idx_ + 1) % period_;
+        auto* next_frame = smps_[sample_idx_];
+        assert(s.size() == len_);
+        if (reverse) {
+            for (int i = 0; i < len_; i++) {
+                next_frame[i] = s[len_ - i - 1];
+            }
+        } else {
+            for (int i = 0; i < len_; i++) {
+                next_frame[i] = s[i];
+            }
+        }
+    }
+
+private:
+    [[nodiscard]] int index_(int idx) const noexcept {
+        assert(idx < period_);
+        //TODO: index optimization
+        return (sample_idx_ + period_ - idx) % period_;
+    }
+
+    const int len_;
+    const int period_;
+    int sample_idx_;   // index of most recent sample
+    arr_real buf_;
+    MutMatView smps_;
+};
+
+class DFTFilterBank
+{
+public:
+    virtual ~DFTFilterBank() = default;
+
+    explicit DFTFilterBank(int num_bands, int decim_factor, int num_taps, bool synthesis = false)
+      : nbands_{num_bands}
+      , ntaps_{num_taps}
+      , decim_{decim_factor}
+      , d_{nbands_ / decim_}
+      , buf_{nbands_, num_taps * decim_}
+      , gsi_{(synthesis ? nbands_ : d_), decim_} {
+    }
+
+    const int nbands_;
+    const int ntaps_;
+    const int decim_;
+    const int d_;
+
+    //TODO: size optimization
+    CircBuffer buf_;
+    CircBuffer gsi_;
+};
+
+arr_real _design_filter(int num_bands, int num_taps) {
+    //TODO: cache last coeffs
+    assert(num_taps % 2 == 0);
+    return design_multirate_fir(1, num_bands, num_taps / 2, 80);
+}
+
+}   // namespace
+
+//--------------------------------------------------------------------------------------------------------
+class ChannelizerImpl : public DFTFilterBank
+{
+public:
+    explicit ChannelizerImpl(std::shared_ptr<const arr_real> filter, int num_bands, int decim_factor, int num_taps)
+      : DFTFilterBank(num_bands, decim_factor, num_taps, false)
+      , flt_{std::move(filter)}
+      , fview_{MatView(*flt_, num_taps, num_bands)}
+      , fft_{fft_plan_r(num_bands)} {
+    }
+
+    [[nodiscard]] arr_cmplx process(const arr_real& x) {
+        DSPLIB_ASSERT(x.size() == d_, "input vector size error");
+
+        gsi_.push(x);
+
+        arr_real convert(nbands_);
+        for (int i = 0; i < decim_; i++) {
+            const auto* gsi = gsi_[decim_ - i - 1];
+            for (int k = 0; k < d_; ++k) {
+                convert[k + i * d_] = gsi[k];
+            }
+        }
+        buf_.push(convert, true);
+
+        // calculate outputs of polyphase filters
+        arr_real pout(nbands_);
+        for (int k = 0; k < ntaps_; k++) {
+            const auto* restrict buf = buf_[decim_ * k];
+            const auto* restrict flt = fview_[k];
+            for (int m = 0; m < nbands_; m++) {
+                pout[nbands_ - m - 1] += flt[m] * buf[m];
+            }
+        }
+
+        return fft_->solve(pout);
+    }
+
+private:
+    std::shared_ptr<const arr_real> flt_;
+    MatView fview_;
+    std::shared_ptr<FftPlanR> fft_;
+};
+
+//--------------------------------------------------------------------------------------------------------------
+class ChannelSynthesizerImpl : public DFTFilterBank
+{
+public:
+    explicit ChannelSynthesizerImpl(std::shared_ptr<const arr_real> filter, int num_bands, int decim_factor,
+                                    int num_taps)
+      : DFTFilterBank(num_bands, decim_factor, num_taps, true)
+      , flt_{std::move(filter)}
+      , fview_{MatView(*flt_, num_taps, num_bands)}
+      , ifft_{ifft_plan_r(num_bands)} {
+    }
+
+    arr_real process(const dsplib::arr_cmplx& x) {
+        DSPLIB_ASSERT(x.size() == nbands_, "input vector size error");
+
+        auto xx = ifft_->solve(x);
+        xx *= nbands_;
+        buf_.push(xx, true);
+
+        // calculate outputs of polyphase filters
+        // TODO: alternative impl for ntaps > nbands
+        arr_real convert(nbands_);
+        for (int k = 0; k < ntaps_; k++) {
+            const auto* restrict buf = buf_[decim_ * k];
+            const auto* restrict flt = fview_[k];
+            for (int m = 0; m < nbands_; m++) {
+                //TODO: inverse filter order
+                convert[m] += flt[nbands_ - m - 1] * buf[m];
+            }
+        }
+        gsi_.push(convert);
+
+        // synthesize final output of filterbank
+        // TODO: inverse order?
+        arr_real out(d_);
+        for (int i = 0; i < decim_; i++) {
+            const auto* gsi = gsi_[decim_ - i - 1];
+            for (int k = 0; k < d_; k++) {
+                out[d_ - k - 1] += gsi[k + i * d_];
+            }
+        }
+
+        //normalize ouput
+        //TODO: more precision, gain error ~ 1 dB
+        out *= nbands_ / decim_;
+        return out;
+    }
+
+private:
+    std::shared_ptr<const arr_real> flt_;
+    MatView fview_;
+    std::shared_ptr<IfftPlanR> ifft_;
+};
+
+//--------------------------------------------------------------------------------------------------------------
+Channelizer::Channelizer(const arr_real& filter, int num_bands, int decim_factor)
+  : Channelizer(std::make_shared<dsplib::arr_real>(filter), num_bands, decim_factor) {
+}
+
+Channelizer::Channelizer(std::shared_ptr<const arr_real> filter, int num_bands, int decim_factor) {
+    const int num_taps = filter->size() / num_bands;
+    DSPLIB_ASSERT(num_taps % 2 == 0, "`num_taps` expected to be even");
+    DSPLIB_ASSERT(num_bands % decim_factor == 0, "only integer ratio M/D supported");
+    DSPLIB_ASSERT(filter->size() == num_bands * num_taps, "filter size must be equal `num_bands * num_taps`");
+    d_ = std::make_unique<ChannelizerImpl>(std::move(filter), num_bands, decim_factor, num_taps);
+}
+
+Channelizer::Channelizer(int num_bands, int decim_factor, int num_taps)
+  : Channelizer(_design_filter(num_bands, num_taps), num_bands, decim_factor) {
+}
+
+arr_cmplx Channelizer::process(const arr_real& x) {
+    return d_->process(x);
+}
+
+int Channelizer::frame_len() const noexcept {
+    return (d_->nbands_ / d_->decim_);
+}
+
+//--------------------------------------------------------------------------------------------------------------
+ChannelSynthesizer::ChannelSynthesizer(const arr_real& filter, int num_bands, int decim_factor)
+  : ChannelSynthesizer(std::make_shared<dsplib::arr_real>(filter), num_bands, decim_factor) {
+}
+
+ChannelSynthesizer::ChannelSynthesizer(std::shared_ptr<const arr_real> filter, int num_bands, int decim_factor) {
+    const int num_taps = filter->size() / num_bands;
+    DSPLIB_ASSERT(num_taps % 2 == 0, "`num_taps` expected to be even");
+    DSPLIB_ASSERT(num_bands % decim_factor == 0, "only integer ratio M/D supported");
+    DSPLIB_ASSERT(filter->size() == num_bands * num_taps, "filter size must be equal `num_bands * num_taps`");
+    d_ = std::make_unique<ChannelSynthesizerImpl>(std::move(filter), num_bands, decim_factor, num_taps);
+}
+
+ChannelSynthesizer::ChannelSynthesizer(int num_bands, int decim_factor, int num_taps)
+  : ChannelSynthesizer(_design_filter(num_bands, num_taps), num_bands, decim_factor) {
+}
+
+arr_real ChannelSynthesizer::process(const arr_cmplx& x) {
+    return d_->process(x);
+}
+
+int ChannelSynthesizer::frame_len() const noexcept {
+    return (d_->nbands_ / d_->decim_);
+}
+
+}   // namespace dsplib
---
+++ tests/subband_test.cpp
@@ -0,0 +1,134 @@
+#include <functional>
+#include <gtest/gtest.h>
+
+#include <dsplib.h>
+#include <memory>
+
+using namespace dsplib;
+
+namespace {
+
+void _frame_apply(const arr_real& sig, int frame_len, std::function<void(const arr_real&)> fn) {
+    int num_frames = sig.size() / frame_len;
+    for (int i = 0; i < num_frames; ++i) {
+        const int t1 = i * frame_len;
+        const int t2 = (i + 1) * frame_len;
+        fn(*sig.slice(t1, t2));
+    }
+}
+
+int _align_arrays(arr_real& x1, arr_real& x2) {
+    const int delay = finddelay(x1, x2);
+    x1 = delayseq(x1, delay);
+    return delay;
+}
+
+}   // namespace
+
+//Check that the output power of the filter is equivalent to the output of the dsp.Channelizer in matlab.
+TEST(Subband, ChannelizerRms) {
+    struct ChanInfo
+    {
+        int nbands;
+        int ntaps;
+        int decim;
+        real_t rms;
+    };
+
+    const int fs = 16000;
+    auto t = arange(10 * fs) / fs;
+    auto x = sin(2 * pi * 1000 * t);
+
+    std::vector<ChanInfo> test_cases = {{8, 12, 2, -12.05}, {32, 8, 4, -12.07}, {64, 8, 1, -21.13}};
+
+    for (auto test : test_cases) {
+        const int nbands = test.nbands;
+        const int ntaps = test.ntaps;
+        const int decim = test.decim;
+
+        auto chan = Channelizer(nbands, decim, ntaps);
+
+        real_t accum = 0;
+        int processed = 0;
+        _frame_apply(x, chan.frame_len(), [&](const arr_real& x) {
+            const auto X = chan(x);
+            accum += sum(abs2(X));
+            processed += x.size();
+        });
+        accum /= processed;
+
+        auto out_rms = pow2db(accum);
+
+        ASSERT_NEAR(test.rms, out_rms, 0.1);
+    }
+}
+
+TEST(Subband, Reconstruction) {
+    int nbands = 512;
+    int ntaps = 2;
+    int decim = 2;
+    int fs = 16000;
+
+    auto chan = Channelizer(nbands, decim, ntaps);
+    auto synth = ChannelSynthesizer(nbands, decim, ntaps);
+
+    auto in = randn(fs * 10);
+    arr_real out;
+    _frame_apply(in, chan.frame_len(), [&](const arr_real& x) {
+        const auto X = chan(x);
+        auto xx = synth(X);
+        out = concatenate(out, xx);
+    });
+
+    int delay = _align_arrays(in, out);
+    auto x1 = *in.slice(-fs, indexing::end);
+    auto x2 = *out.slice(-fs, indexing::end);
+    auto [_, R] = gccphat(x1, x2);
+    auto aR = abs(R);
+    auto corr_peak = max(aR);
+    ASSERT_GE(corr_peak, 0.99);
+
+    //weak correlation side lobes
+    auto max_lobes = max(aR.slice(1, indexing::end));
+    ASSERT_LE(max_lobes, 0.005);
+    //TODO: check auto-corr function
+}
+
+TEST(Subband, Oversampled2x) {
+    const int nbands = 512;
+    const int ntaps = 4;
+    const int D = 2;
+
+    const int fs = 16000;
+
+    auto wp = 1.0 / nbands;
+    const auto h = fir1((ntaps * nbands - 1), wp);
+    const auto hptr = std::make_shared<const arr_real>(h);
+
+    auto chan = Channelizer(hptr, nbands, D);
+    auto synth = ChannelSynthesizer(hptr, nbands, D);
+
+    auto in = randn(fs * 10);
+    arr_real out;
+    _frame_apply(in, chan.frame_len(), [&](const arr_real& x) {
+        auto X = chan.process(x);
+        ASSERT_EQ(X.size(), nbands);
+        out |= synth.process(X);
+    });
+
+    //compensate delay
+    //TODO: add delay() to Channelizer
+    int delay = _align_arrays(in, out);
+    ASSERT_NEAR(delay, (h.size() - chan.frame_len()), 1);
+
+    //power is equal
+    arr_real in_d = in.slice(-fs, indexing::end);
+    arr_real out_d = out.slice(-fs, indexing::end);
+    auto in_rms = mag2db(rms(in_d));
+    auto out_rms = mag2db(rms(out_d));
+    ASSERT_NEAR(in_rms, out_rms, 2);
+
+    //xcorr(in, out) must be near
+    auto [_, corr] = gccphat(in_d, out_d);
+    ASSERT_GE(abs(max(corr)), 0.98);
+}
