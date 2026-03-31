decfee507129f2703431c4c55fb0f838c65dcd37
Vitaly Yulis <vitaly.yulis@gmail.com>
2026-03-31 12:59:47 +0300

    Fix: refactoring Delay class

--- include/dsplib/delay.h
+++ include/dsplib/delay.h
@@ -1,27 +1,30 @@
 #pragma once
 
 #include <dsplib/array.h>
+#include <utility>
 
 namespace dsplib {
 
 //Delay input signal by fixed samples
 //TODO: add fractional delay
+//TODO: rename to `FixedDelay`?
 template<typename T>
 class Delay
 {
 public:
     explicit Delay(int length)
       : nd_{length}
-      , buf_(length) {
+      , cbuf_(length) {
+        DSPLIB_ASSERT(length >= 0, "Delay length must be non-negative");
     }
 
-    explicit Delay(const dsplib::base_array<T>& initial)
+    explicit Delay(span_t<T> initial)
       : nd_{initial.size()}
-      , buf_{initial} {
+      , cbuf_{initial} {
     }
 
-    dsplib::base_array<T> process(span_t<T> x) {
-        dsplib::base_array<T> dx(x);
+    base_array<T> process(span_t<T> x) {
+        base_array<T> dx(x);
         this->process(inplace(dx));
         return dx;
     }
@@ -32,23 +35,23 @@ public:
         }
         auto x = ix.get();
         for (int i = 0; i < x.size(); ++i) {
-            std::swap(buf_[pos_], x[i]);
+            std::swap(cbuf_[pos_], x[i]);
             pos_ = (pos_ + 1) % nd_;
         }
     }
 
-    dsplib::base_array<T> operator()(span_t<T> x) {
+    base_array<T> operator()(span_t<T> x) {
         return this->process(x);
     }
 
     [[nodiscard]] int length() const noexcept {
-        return buf_.size();
+        return nd_;
     }
 
 private:
     int nd_{0};
     int pos_{0};
-    dsplib::base_array<T> buf_;
+    base_array<T> cbuf_;
 };
 
 using DelayReal = Delay<real_t>;
