ef40c462079b9f0bd9e677458727745881e2a0b5
Vitaly Yulis <vitaly.yulis@gmail.com>
2026-03-31 12:17:50 +0300

    Fix: use cyclic buffer for Delay class, add zero length support

--- include/dsplib/delay.h
+++ include/dsplib/delay.h
@@ -5,32 +5,50 @@
 namespace dsplib {
 
 //Delay input signal by fixed samples
+//TODO: add fractional delay
 template<typename T>
 class Delay
 {
 public:
     explicit Delay(int length)
-      : _buffer(length) {
+      : nd_{length}
+      , buf_(length) {
     }
 
     explicit Delay(const dsplib::base_array<T>& initial)
-      : _buffer{initial} {
+      : nd_{initial.size()}
+      , buf_{initial} {
     }
 
     dsplib::base_array<T> process(span_t<T> x) {
-        //TODO: use cycle buffer
-        const int nd = _buffer.size();
-        const auto tmp = concatenate(_buffer, x);
-        _buffer.slice(0, nd) = tmp.slice(tmp.size() - nd, tmp.size());
-        return tmp.slice(0, x.size());
+        dsplib::base_array<T> dx(x);
+        this->process(inplace(dx));
+        return dx;
+    }
+
+    void process(inplace_span_t<T> ix) {
+        if (nd_ == 0) {
+            return;
+        }
+        auto x = ix.get();
+        for (int i = 0; i < x.size(); ++i) {
+            std::swap(buf_[pos_], x[i]);
+            pos_ = (pos_ + 1) % nd_;
+        }
     }
 
     dsplib::base_array<T> operator()(span_t<T> x) {
         return this->process(x);
     }
 
+    [[nodiscard]] int length() const noexcept {
+        return buf_.size();
+    }
+
 private:
-    dsplib::base_array<T> _buffer;
+    int nd_{0};
+    int pos_{0};
+    dsplib::base_array<T> buf_;
 };
 
 using DelayReal = Delay<real_t>;
